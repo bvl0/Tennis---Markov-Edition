33f257d4e30cb5e5a6342ab493b8d22c3f91ef83
bvl0 <icaro1561@outlook.com>
2022-03-05 18:45:35 -0300

    simplesmente esquecio do commit inicial

---
+++ tennis.py
@@ -0,0 +1,39 @@
+import random
+def game(p, tiebrake):
+    points = [0,0]
+
+    limit = 7 if tiebrake else 4
+    
+    while max(points) < limit or (max(points) - min(points)) <= 1:
+        if random.random() <= p:
+            points[0] += 1
+        else:
+            points[1] +=1
+    
+    print("points = " + str(points))
+    
+    if points[0] > points[1]:
+        return 0
+    else:
+        return 1
+
+def set(p):
+    games = [0,0]
+
+    while max(games) < 7:
+        if max(games) == 6 and max(games) - min(games) >=2:
+            break
+        
+        if games[0] == 6 and games[1] == 6:
+            print("tiebrake")
+            tiebrake = True
+        else:
+            tiebrake = False
+
+        winner = game(p, tiebrake)
+        games[winner] += 1
+    
+    print("games = " + str(games))
+
+
+set(0.5)
